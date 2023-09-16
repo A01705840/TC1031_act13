aa1a0070af741665bf47a784106ba6cdcd8787b9
Maria José Soto <A01705840@tec.mx>
2023-09-15 22:18:47 -0600

    Añadir archivo de programa

---
+++ tc1031_act3.cpp
@@ -0,0 +1,28 @@
+// =========================================================
+// File: TC1031_act3.cpp
+// Author: María José Soto Castro - A01705840
+// Date: 14/09/2023
+// =========================================================
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// =========================================================
+// main
+
+int n; //
+int m;
+
+int *ADN;
+cin >> n;
+cin >> m;
+
+ADN = new int[n];
+
+for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+        if cadena
+    }
+}
